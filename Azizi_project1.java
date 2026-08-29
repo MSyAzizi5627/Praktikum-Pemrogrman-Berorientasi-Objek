b57d8c0959f34df8908683e15728ddde54b5ac8d
Muhammad Syafiq Azizi <shisui538@gmail.com>
2026-08-29 18:36:06 +0700

    Add files via upload

---
+++ Azizi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.azizi_project1;
+
+/**
+ *
+ * @author acer
+ */
+public class Azizi_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Syafiq Azizi");
+        System.out.println("NPM: 2505060045");
+        System.out.println("Alamat: Magelang");
+        System.out.println("No_HP: 085655457037");
+    }
+}
